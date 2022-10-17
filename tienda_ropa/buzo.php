24dffce1eeb68e25692b412f17577df42ed8dc18
BELEN BUSSI <bussibelen1@gmail.com>
2022-10-17 20:26:06 -0300

    V2 tienda

--- tienda_ropa/buzo.php
+++ tienda_ropa/buzo.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listado Ropa</title>
+    <title>Buzos</title>
   </head>
   <body>
     <h1>Tienda de ropa</h1>
